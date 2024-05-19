6f4157003ec0b5dd364df47925592c18e0221047
Sidahmed Belkadi <sidahmed.belkadi0@gmail.com>
2024-05-19 16:07:27 +0100

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:todo_app/src/todo_app.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.delayed(const Duration(seconds: 5));
+
+  FlutterNativeSplash.remove();
   runApp(const TodoApp());
 }
