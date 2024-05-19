210ebe6aa508ba0ab5b1efdce0c12991829b7cdd
Sidahmed Belkadi <sidahmed.belkadi0@gmail.com>
2024-05-19 14:51:06 +0100

    Added DM Sans font family to text theme.

--- lib/src/config/themes/app_theme.dart
+++ lib/src/config/themes/app_theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData appTheme() {
   return ThemeData(
     brightness: Brightness.light,
+    textTheme: GoogleFonts.dmSansTextTheme(),
     colorScheme: ColorScheme.fromSeed(
       seedColor: Colors.deepPurple,
       brightness: Brightness.light,
